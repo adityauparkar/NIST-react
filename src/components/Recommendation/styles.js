a8a92f7d5f95de1cf0899a7b8d54409a6a0dcc22
SharenOliver <64525617+SharenOliver@users.noreply.github.com>
2020-05-15 18:12:26 +1000

    I have updated it

---
+++ src/components/Recommendation/styles.js
@@ -0,0 +1,54 @@
+body
+  {
+    margin: 0;
+    font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji";
+    font-size: .88rem;
+    font-weight: 400;
+    line-height: 1.5;
+    color: #495057;
+    text-align: left;
+    background-color: #fff;
+     }
+  box-sizing: border-box;
+     }
+body
+    {
+      display: block;
+    }
+:root
+    {
+      --blue: #007bff;
+      --indigo: #6610f2;
+      --purple: #6f42c1;
+      --pink: #e83e8c;
+      --red: #dc3545;
+      --orange: #fd7e14;
+      --yellow: #ffc107;
+      --green: #28a745;
+      --teal: #20c997;
+      --cyan: #17a2b8;
+      --white: #fff;
+      --gray: #6c757d;
+      --gray-dark: #343a40;
+      --primary: #3f6ad8;
+      --secondary: #6c757d;
+      --success: #3ac47d;
+      --info: #16aaff;
+      --warning: #f7b924;
+      --danger: #d92550;
+      --light: #eee;
+      --dark: #343a40;
+      --focus: #444054;
+      --alternate: #794c8a;
+      --breakpoint-xs: 0;
+      --breakpoint-sm: 576px;
+      --breakpoint-md: 768px;
+      --breakpoint-lg: 992px;
+      --breakpoint-xl: 1200px;
+      --font-family-sans-serif: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+      --font-family-monospace: SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace;
+    }
+html
+    {
+    -webkit-tap-highlight-color: rgba(0,0,0,0);
+    }
